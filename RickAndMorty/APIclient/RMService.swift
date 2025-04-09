2f8fc7d17163f26cfe710369eda5c62e073bb73b
L G <lg@Ls-Mac-mini.local>
2025-04-09 17:54:55 +0200

    Created API Request, URL builder

--- RickAndMorty/APIclient/RMService.swift
+++ RickAndMorty/APIclient/RMService.swift
@@ -20,7 +20,11 @@ final class RMService {
     /// Send RM API call
     /// - Parameters:
     ///   - request: request instance
+    ///   - type: the type of object we expect to get back
     ///   - completion: callback with data or error
-    public func execute(_ request:RMRequest, completion: @escaping () -> Void){}
+    public func execute<T: Codable>(
+        _ request:RMRequest,
+        expecting typle : T.Type,
+        completion: @escaping (Result<T, Error>) -> Void){}
     
 }
